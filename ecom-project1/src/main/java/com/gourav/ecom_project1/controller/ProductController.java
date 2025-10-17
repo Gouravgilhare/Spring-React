c4e4b7d2db35253ad30a9f91a41ad02981bf93eb
Gouravgilhare <gouravgilhare4@gmail.com>
2025-10-18 00:12:34 +0530

    18-10-25

--- ecom-project1/src/main/java/com/gourav/ecom_project1/controller/ProductController.java
+++ ecom-project1/src/main/java/com/gourav/ecom_project1/controller/ProductController.java
@@ -5,9 +5,12 @@ import com.gourav.ecom_project1.model.Product;
 import com.gourav.ecom_project1.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @CrossOrigin
@@ -38,4 +41,59 @@ public class ProductController {
     }
 
 
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart  Product product, @RequestPart MultipartFile imageFile){
+
+        try {
+            Product product1 = service.addProduct(product, imageFile);
+            return new ResponseEntity<>(product1, HttpStatus.CREATED);
+        }catch(Exception e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/product/{productId}/image")
+    public ResponseEntity<byte[]>getImageByProduct(@PathVariable int prodId){
+
+        Product product  = service.getProductById(prodId);
+        byte[] imageFile = product.getImageDate();
+
+        return ResponseEntity.ok().contentType(MediaType.valueOf(product.getImageType())).body(imageFile);
+    }
+
+    @PutMapping("/product/{id}")
+    public ResponseEntity<String> updateProduct(@PathVariable int id, @RequestPart Product product, @RequestPart MultipartFile imageFile) throws IOException {
+
+        Product product1 = null;
+        try {
+            product1 = service.updateProduct(id, product, imageFile);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        if (product1 != null) {
+            return new ResponseEntity<>("Updated", HttpStatus.OK);
+        }
+        else{
+
+            return new ResponseEntity<>("Failed to update", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
+    @DeleteMapping("/product/{id}")
+    public ResponseEntity<String>deleteProduct(@PathVariable int id){
+        Product product = service.getProductById(id);
+        if(product!= null)
+        {
+            service.deleteProduct(id);
+            return new ResponseEntity<>("Deleted Successfully", HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>("Product not Found", HttpStatus.NOT_FOUND);
+        }
+
+    }
+
 }
+
+
--- ecom-project1/src/main/java/com/gourav/ecom_project1/model/Product.java
+++ ecom-project1/src/main/java/com/gourav/ecom_project1/model/Product.java
@@ -2,10 +2,7 @@ package com.gourav.ecom_project1.model;
 
 import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -28,10 +25,16 @@ public class Product {
     private String desc;
     private String catagory;
     private BigDecimal price;
-
-    @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="dd-mm--yyyy")
     private Date releaseDate;
     private boolean available;
     private int quantity;
+
+    private String imageName;
+    private String imageType;
+
+    @Lob
+    private byte[] imageDate;
+
+
 }
 
